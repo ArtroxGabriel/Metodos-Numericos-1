e3e524718050323545f451bc71425152aef6e127
ArtroxGabriel <artroxgabriel@gmail.com>
2024-11-06 11:45:52 -0300

    menu to test

--- tarefa1/main.cpp
+++ tarefa1/main.cpp
@@ -1,10 +1,54 @@
 #include "gabrigas_math.hpp"
 #include <iostream>
+#include <ostream>
+#include <string>
+
+using namespace std;
+
+void menu();
 
 int main() {
-  std::cout << decimal_to_binary(27) << std::endl;
-  std::cout << binary_to_decimal(0b11011) << std::endl;
-  std::cout << binary_to_decimal_optimized(0b11011) << std::endl;
+  menu();
 
   return 0;
+}
+
+void menu() {
+  int choice;
+  string bin;
+  string dec;
+  while (true) {
+    cout << "\tEscolha a função a se testar" << endl;
+    cout << "\t1 - Conversão Decimal para Binario" << endl;
+    cout << "\t2 - Conversão Binario para Decimal" << endl;
+    cout << "\t3 - Conversão Binario para Decimal Otimizado" << endl;
+    cout << "\t0 - sair" << endl;
+    cin >> choice;
+
+    switch (choice) {
+    case 0:
+      return;
+    case 1:
+      cout << "Digite o numero decimal para conversão" << endl;
+      cin >> dec;
+
+      cout << decimal_to_binary(dec) << endl;
+      break;
+    case 2:
+      cout << "Digite o numero binario para conversão" << endl;
+      cin >> bin;
+
+      cout << binary_to_decimal(bin /* stoi(bin, nullptr, 2) */) << endl;
+      break;
+    case 3:
+      cout << "Digite o numero binario para conversão" << endl;
+      cin >> bin;
+
+      cout << binary_to_decimal_optimized(bin) << endl;
+      break;
+    default:
+      cout << "Não é uma opção" << endl;
+    }
+    cout << endl;
+  }
 }
